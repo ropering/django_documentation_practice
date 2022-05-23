36d3e0c41ae35dbfce96d0d9fa62974abe95a4a9
ropering <bonoboss1028@gmail.com>
2022-05-23 18:18:42 +0900

    [feat] Add views.py

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import *
 
 
 def index(request):
-    return HttpResponse("Hello World~")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Questions {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"results of questions {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"voting in questions {question_id}")
+
+

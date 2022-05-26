0ab00789b226f31a4e7d3015768db50c3c531c0e
ropering <bonoboss1028@gmail.com>
2022-05-26 22:28:39 +0900

    [feat] Question 에서 Choice도 함께 생성할 수 있도록

--- polls/admin.py
+++ polls/admin.py
@@ -3,11 +3,20 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3  # 선택 가능 폼 갯수 (추가 가능)
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {
+            'fields': ['pub_date'],
+            'classes': ['collapse']  # 감추기
+        }),
     ]
+    inlines = [ChoiceInline]  # Choice 추가하기
 
 
 admin.site.register(Question, QuestionAdmin)

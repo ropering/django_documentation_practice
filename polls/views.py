7eaf84271a04acac2e976be6708b90fa72083bdd
ropering <bonoboss1028@gmail.com>
2022-05-31 23:39:20 +0900

    doc: Add 궁금한 것, 정리

--- polls/views.py
+++ polls/views.py
@@ -8,18 +8,18 @@ from .models import *
 
 
 # Index page
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
+class IndexView(generic.ListView):  # ListView 와 DetailView 차이가 무엇인가?
+    template_name = 'polls/index.html'  # 이렇게 정해진(약속된) 변수에 값을 기입해야 하는 것 같다
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         return Question.objects.filter(
-            pub_date__lte=timezone.now()
+            pub_date__lte=timezone.now()  # 생성 날짜가 지금인 것만 가져오는건가?
         ).order_by('-pub_date')[:5]
 
 
 # polls/<int:pk>
-class DetailView(generic.DetailView):
+class DetailView(generic.DetailView):  # DetailView
     model = Question
     template_name = 'polls/detail.html'
 
@@ -35,7 +35,7 @@ class ResultsView(generic.DetailView):
 
 # polls/<int:question_id>/vote/
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)  # 존재하면 객체를 가져오고 아니면 404 에러 발생
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
